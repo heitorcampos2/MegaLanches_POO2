e40c5c91b0e9e2e0f74c33567414b9ce6c3a808f
Heitor Campos <Heitor Campos@HeiThor>
2019-11-28 12:48:24 -0200

    preparação para gerar instalador .exe ou .msi

--- src/ui/main/Main.java
+++ src/ui/main/Main.java
@@ -7,12 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Heitor
- * @author david
- *
- */
 public class Main extends Application {
 
     @Override
